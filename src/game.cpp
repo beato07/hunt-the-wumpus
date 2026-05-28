fd5cf947d4dfe2773484c27acfdf3e1a89a647fa
Egor <egor.pyuretskiy@gmail.com>
2026-05-28 22:39:09 +0300

    update rules

--- src/game.cpp
+++ src/game.cpp
@@ -5,6 +5,9 @@
 #include <random>
 #include <iostream>
 
+#include <string>
+
+
 Game::Game()
 {
 	setupEntities();
@@ -33,7 +36,7 @@ void Game::help()
 	char answer = '\0';
 	do
 	{
-		std::cout << "Instructions? Y/N\n";
+		std::cout << "Instructions (Y-N)? ";
 		std::cin >> answer;
 
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -54,18 +57,65 @@ void Game::help()
 
 void Game::printRules()
 {
-	std::cout << "\nYour goal is to find and kill the Wumpus.\n";
-	std::cout << "There are 3 different types of hazards :\n";
-	std::cout << "Caves, which cause a draft nearby and kill you if you fall\n";
-	std::cout << "Bats, which cause a flapping noise and carry you somewhere random\n";
-	std::cout << "And the Wumpus. Smelly, unbathed, dirty, it will give off a terrible smell\n";
-	std::cout << "The Wumpus won't fall down the bottomless pit nor will bats be able to move him\n";
-	std::cout << "Also, if you try to shoot an arrow and miss, the wumpus will move to a different location\n\n";
+	std::cout << "WELCOME TO 'HUNT THE WUMPUS'\n";
+	std::cout << "  THE WUMPUS LIVES IN A CAVE OF 20 ROOMS. EACH ROOM\n";
+	std::cout << "HAS 3 TUNNELS LEADING TO OTHER ROOMS. (LOOK AT A\n";
+	std::cout << "DODECAHEDRON TO SEE HOW THIS WORKS-IF YOU DON'T KNOW\n";
+	std::cout << "WHAT A DODECAHEDRON IS, ASK SOMEONE)\n";
+
+	std::string pass;
+	std::getline(std::cin, pass);
+
+	std::cout << '\n';
+	std::cout << "     HAZARDS:\n";
+	std::cout << "BOTTOMLESS PITS - TWO ROOMS HAVE BOTTOMLESS PITS IN THEM\n";
+	std::cout << "    IF YOU GO THERE, YOU FALL INTO THE PIT (& LOSE!)\n";
+	std::cout << "SUPER BATS - TWO OTHER ROOMS HAVE SUPER BATS. IF YOU\n";
+	std::cout << "    GO THERE, A BAT GRABS YOU AND TAKES YOU TO SOME OTHER\n";
+	std::cout << "    ROOM AT RANDOM. (WHICH MIGHT BE TROUBLESOME)\n";
+
+	std::getline(std::cin, pass);
+
+	std::cout << '\n';
+	std::cout << "    WUMPUS:\n";
+	std::cout << "THE WUMPUS IS NOT BOTHERED BY THE HAZARDS (HE HAS SUCKER\n";
+	std::cout << "FEET AND IS TOO BIG FOR A BAT TO LIFT). USUALLY\n";
+	std::cout << "HE IS ASLEEP. TWO THINGS WAKE HIM UP: YOUR ENTERING\n";
+	std::cout << "HIS ROOM OR YOUR SHOOTING AN ARROW.\n";
+	std::cout << "    IF THE WUMPUS WAKES, HE MOVES (P=.75) ONE ROOM\n";
+	std::cout << "OR STAYS STILL (P=.25). AFTER THAT, IF HE IS WHERE YOU\n";
+	std::cout << "ARE, HE EATS YOU UP (& YOU LOSE!)\n";
+
+	std::getline(std::cin, pass);
+
+	std::cout << '\n';
+	std::cout << "    YOU:\n";
+	std::cout << "EACH TURN YOU MAY MOVE OR SHOOT A CROOKED ARROW\n";
+	std::cout << "  MOVING: YOU CAN GO ONE ROOM (THRU ONE TUNNEL)\n";
+	std::cout << "  ARROWS: YOU HAVE 5 ARROWS. YOU LOSE WHEN YOU RUN OUT.\n";
+	std::cout << "  EACH ARROW CAN GO FROM 1 TO 5 ROOMS. YOU AIM BY TELLING\n";
+	std::cout << "  THE COMPUTER THE ROOM#S YOU WANT THE ARROW TO GO TO.\n";
+	std::cout << "  IF THE ARROW CAN'T GO THAT WAY(IE NO TUNNEL) IT MOVES\n";
+	std::cout << "  AT RANDOM TO THE NEXT ROOM.\n";
+	std::cout << "    IF THE ARROW HITS THE WUMPUS, YOU WIN.\n";
+	std::cout << "    IF THE ARROW HITS YOU, YOU LOSE.\n";
+
+	std::getline(std::cin, pass);
+
+	std::cout << '\n';
+	std::cout << "    WARNINGS:\n";
+	std::cout << "     WHEN YOU ARE ONE ROOM AWAY FROM WUMPUS OR HAZARD,\n";
+	std::cout << "    THE COMPUTER SAYS:\n";
+	std::cout << " WUMPUS-  'I SMELL A WUMPUS'\n";
+	std::cout << " BAT   -  'BATS NEARBY'\n";
+	std::cout << " PIT   -  'I FEEL A DRAFT'\n\n";
 }
 
 void Game::run()
 {
 	help();
+
+	std::cout << "HUNT THE WUMPUS";
 
 	bool isRunning = true;
 	while (isRunning)
